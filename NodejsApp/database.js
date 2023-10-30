5645e08a25b9fe6b5470c35375611fb8c0d4a1c2
ChanuWeingarten <c0583260972@gmail.com>
2023-10-30 16:42:15 +0200

    starting

---
+++ NodejsApp/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const MONGODB_URI = 'mongodb://mongo-db:27017/pucsd';
+
+mongoose.connect(MONGODB_URI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+  .then(() => {
+    console.log("Successfully connected to MongoDB.");
+  }).catch(err => {
+    console.log('Could not connect to MongoDB.');
+    process.exit();
+  });
+
+
+
+ 
